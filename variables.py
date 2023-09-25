919d898dcb697b78da08a6b51df63768649d6896
Ramnik Layal <ramniklayal@gmail.com>
2023-09-25 18:28:52 -0400

    Added variables.py

---
+++ variables.py
@@ -0,0 +1,10 @@
+# Hashtag is single line comment, 3x single/double quotes are docstrings/mutli-line comments
+# Variable is simply a container for a data value
+
+'''
+For example,
+x = 7
+
+x is the variable (container)
+7 is the data value
+'''
