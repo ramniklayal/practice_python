ac67f3e32b7df9038ec59156b3ee78736319eed8
Ramnik Layal <ramniklayal@gmail.com>
2023-09-26 20:44:10 -0400

    Changed variables.py

--- variables.py
+++ variables.py
@@ -1,3 +1,5 @@
+# I am learning python from Traversy Media Python Crash Course For Begineers on Youtube
+
 # Hashtag is single line comment, 3x single/double quotes are docstrings/mutli-line comments
 # Variable is simply a container for a data value
 
@@ -8,3 +10,20 @@ x = 7
 x is the variable (container)
 7 is the data value
 '''
+
+'''
+Variables rules:
+- Case sensitive
+- Must start with a letter or _
+- Can have numbers but can not start with one
+'''
+
+# x = 1           # int
+# y = 2.5         # float
+# name = 'Marty'  # string
+# is_chill = True  # boolean
+
+# Multiple assignment same as above
+x, y, name, is_chill = (1, 2.5, 'Marty', True)
+
+print(x, y, name, is_chill)
