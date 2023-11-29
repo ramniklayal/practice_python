11f5a00f9cadd91cad4494b56e5cbfdd7d43b251
Ramnik Layal <ramniklayal@gmail.com>
2023-11-28 21:48:54 -0500

    Rename dir and update variables.py

---
+++ traversy-media/app.py
@@ -0,0 +1,8 @@
+#Basic programs to get started with python
+
+print("Hello World")
+
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
---
+++ traversy-media/variables.py
@@ -0,0 +1,15 @@
+# Variables are containers for values, which can be of any type
+
+# x = 27              # integer   int
+# y = 4.5             # decimal   float     
+# likes_bong = True   # boolean   bool
+# name = 'bob'        # string    str
+
+# Multi Assignment
+x, y, likes_bong, name = (27, 4.5, True, 'bob')
+
+# Casting
+x = str(x)
+y = int(y)
+
+print(type(y),y)
