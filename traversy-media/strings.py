5b7e53ef3a6c8754ed824503f2bb6474d98800ff
Ramnik Layal <ramniklayal@gmail.com>
2023-11-30 14:52:05 -0500

    Add strings.py

---
+++ traversy-media/strings.py
@@ -0,0 +1,24 @@
+name = 'Bob'
+age = 23
+
+# Concatenate
+# print('Hello my name is ' + name + ' and my age is ' + str(age))
+
+
+# String formatting
+
+# Arguements by position
+# print('Hello my name is {name} and my age is {age}'.format(name=name, age=age))
+
+# F-strings (ver 3.6+)
+# print(f'Hello my name is {name} and my age is {age}')
+
+
+# String methods
+
+s = 'hello world'
+
+# To capitalize
+print(s.capitalize())
+
+# Many other methaods like replace, swapcase, len, startswith. Look up online as needed
