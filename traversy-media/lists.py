81986fd5942c83e038122c5ffe8ee3337bca28e6
Ramnik Layal <ramniklayal@gmail.com>
2023-12-01 19:12:06 -0500

    Add lists.py

---
+++ traversy-media/lists.py
@@ -0,0 +1,42 @@
+# Create list
+numbers = [1, 2, 3, 4, 5]
+fruits = ['Apples', 'Oranges', 'Grapes', 'Pears']
+
+# Use a constructor
+# numbers2 = list((1, 2, 3, 4, 5))
+'''
+# Get a value
+print(fruits[1])
+
+# Get length
+print(len(fruits))
+
+# Append to list
+fruits.append('Mangoes')
+print(fruits)
+
+# Remove from list
+fruits.remove('Grapes')
+print(fruits)
+'''
+
+# Insert into pos
+# fruits.insert(2, 'Strawberries')
+# print(fruits)
+
+# Remove with pop
+# fruits.pop(2)
+
+# Reverse list
+# fruits.reverse()
+
+# Sort
+# fruits.sort()
+
+# Reverse sort
+# fruits.sort(reverse=True)
+
+# Change value
+fruits[0] = 'Apricots'
+
+print(fruits)
